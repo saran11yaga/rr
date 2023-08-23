bfc3c7cf32b31936ab2d5b8d93f3534e0a1e403f
saranya <saranya@prabalinfo.co.in>
2023-08-23 10:11:09 +0530

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">User Dashboard</div>
 
                 <div class="card-body">
                      Hi there, regular user
--- resources/views/writer.blade.php
+++ resources/views/writer.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Writer Dashboard</div>
 
                 <div class="card-body">
                     Hi there, awesome writer
